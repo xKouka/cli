b6836219a389bde1a20ccc3291f351f3ddd4a13a
Oscar Aguiar <159394490+xKouka@users.noreply.github.com>
2025-06-03 13:53:28 -0400

    fixed finish

--- docs/es/fix-md031.ps1
+++
@@ -1,8 +0,0 @@
-(Get-Content .\README.md) |
-ForEach-Object {
-    if ($_ -match '^\s*```') {
-        "`n$_`n"
-    } else {
-        $_
-    }
-} | Set-Content .\README.md
--- docs/es/fix-md032.ps1
+++
@@ -1,9 +0,0 @@
-# filepath: c:\Users\oagui\Documents\GitHub\cli\docs\es\fix-md032.ps1
-(Get-Content .\README.md) |
-ForEach-Object {
-    if ($_ -match '^\s*[-*+] ') {
-        "`n$_`n"
-    } else {
-        $_
-    }
-} | Set-Content .\README.md
